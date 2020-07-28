78c5779cb3a630e50aff1076f8258b006437d6ce
raspberrypisig <raspberrypisig@gmail.com>
2020-07-28 03:39:00 -0400

    Update 005-etcd-fix.sh

--- openstack/005-etcd-fix.sh
+++ openstack/005-etcd-fix.sh
@@ -2,3 +2,19 @@
 
 echo "ETCD_UNSUPPORTED_ARCH=arm64" >> /etc/default/etcd
 ETCD_UNSUPPORTED_ARCH=arm64 apt install -f
+
+#  /etc/default/etcd
+cat<<EOF
+ETCD_NAME="controller"
+ETCD_DATA_DIR="/var/lib/etcd"
+ETCD_INITIAL_CLUSTER_STATE="new"
+ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster-01"
+ETCD_INITIAL_CLUSTER="controller=http://10.0.0.11:2380"
+ETCD_INITIAL_ADVERTISE_PEER_URLS="http://10.0.0.11:2380"
+ETCD_ADVERTISE_CLIENT_URLS="http://10.0.0.11:2379"
+ETCD_LISTEN_PEER_URLS="http://0.0.0.0:2380"
+ETCD_LISTEN_CLIENT_URLS="http://10.0.0.11:2379"
+EOF
+
+# systemctl enable etcd
+# systemctl restart etcd
