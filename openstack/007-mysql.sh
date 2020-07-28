597a3e286d5c6b5abee7e6b09683aa099b0e462c
raspberrypisig <raspberrypisig@gmail.com>
2020-07-28 03:32:16 -0400

    Create 007-mysql.sh

---
+++ openstack/007-mysql.sh
@@ -0,0 +1,13 @@
+cat<<EOF > etc/mysql/mariadb.conf.d/99-openstack.cnf
+[mysqld]
+bind-address = 10.0.0.11
+
+default-storage-engine = innodb
+innodb_file_per_table = on
+max_connections = 4096
+collation-server = utf8_general_ci
+character-set-server = utf8
+EOF
+
+service mysql restart
+mysql_secure_installation
