761f04ca30ceb02058a50b21d28ab11ca3689806
raspberrypisig <raspberrypisig@gmail.com>
2020-07-27 23:39:25 -0400

    Update 001-networkconfig.sh

--- openstack/001-networkconfig.sh
+++ openstack/001-networkconfig.sh
@@ -1,5 +1,41 @@
 #!/usr/bin/env bash
 
+set -x
+
+SSID=mycrib
+PSK=peachspeak38
+CONTROLLER=192.168.20.99
+
 cat<<EOF >> /etc/hosts
-192.168.20.99 controller
+$CONTROLLER controller
 EOF
+
+wpa_passphrase $SSID $PSK > /etc/wpa_supplicant.conf
+
+cat<<EOF > /etc/systemd/system/wpa_supplicant.service
+[Unit]
+Description=WPA supplicant
+Before=network.target
+After=dbus.service
+Wants=network.target
+IgnoreOnIsolate=true
+
+[Service]
+Type=dbus
+BusName=fi.w1.wpa_supplicant1
+ExecStart=/sbin/wpa_supplicant -u -s -O /run/wpa_supplicant
+
+[Install]
+WantedBy=multi-user.target
+Alias=dbus-fi.w1.wpa_supplicant1.service
+EOF
+
+cat<<EOF > /etc/systemd/network/20-dhclient.network
+[Match]
+Name=wlan0
+
+[Network]
+DHCP=ipv4
+EOF
+
+
