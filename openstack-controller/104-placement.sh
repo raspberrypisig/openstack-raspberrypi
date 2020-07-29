abba75c89ea243737f8618c917af6316e7f485b2
raspberrypisig <raspberrypisig@gmail.com>
2020-07-29 05:07:55 -0400

    Create 104-placement.sh

---
+++ openstack-controller/104-placement.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+mysql <<EOF
+CREATE DATABASE placement;
+GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'localhost' IDENTIFIED BY '$PLACEMENT_DBPASS';
+GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'%' IDENTIFIED BY '$PLACEMENT_DBPASS';
+
+EOF
+
+openstack user create --domain default --password $PLACEMENT_PASS placement
+openstack role add --project service --user placement admin
+openstack service create --name placement --description "Placement API" placement
+openstack endpoint create --region RegionOne placement public http://controller:8778
+openstack endpoint create --region RegionOne placement internal http://controller:8778
+openstack endpoint create --region RegionOne placement admin http://controller:8778
+
+apt install -y placement-api
+
+sed -i '/^\[placement_database\]/{N;s/\n/\n#/}' /etc/placement/placement.conf
+sed -i "/^\[placement_database\]/a connection = mysql+pymysql://glance:$GLANCE_DBPASS@controller/glance" /etc/placement/placement.conf
+
+
+
+
+service apache2 restart
+su -s /bin/sh -c "placement-manage db sync" placement
