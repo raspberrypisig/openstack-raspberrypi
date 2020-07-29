fa4818d49ab3cf32bd61901765804be8b96cad85
raspberrypisig <raspberrypisig@gmail.com>
2020-07-28 23:50:40 -0400

    Create 100-installprereq.sh

---
+++ openstack-controller/100-installprereq.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cat <<EOF | 
+keystone
+glance
+placement-api
+nova-api 
+nova-conductor 
+nova-novncproxy 
+nova-scheduler
+neutron
+openstack-dashboard
+neutron-server 
+neutron-plugin-ml2 
+neutron-linuxbridge-agent 
+neutron-l3-agent 
+neutron-dhcp-agent 
+neutron-metadata-agent 
+python3-neutronclient 
+EOF
+
+while read line
+do
+  apt install -y $line
+done
