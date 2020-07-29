f93f0138ef6de84c92eb0fe0cd24c6d832c6477f
raspberrypisig <raspberrypisig@gmail.com>
2020-07-28 21:53:02 -0400

    Create set_env_vars.sh

---
+++ set_env_vars.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Passwords
+export DEFAULTPASSWORD=password
+
+export ADMIN_PASS=password
+export CINDER_DBPASS=password
+export CINDER_PASS=password
+export DASH_DBPASS=password
+export DEMO_PASS=password
+export GLANCE_DBPASS=password
+export GLANCE_PASS=password
+export KEYSTONE_DBPASS=password
+export METADATA_SECRET=password
+export NEUTRON_DBPASS=password
+export NEUTRON_PASS=password
+export NOVA_DBPASS=password
+export NOVA_PASS=password
+export PLACEMENT_PASS=password
+export RABBIT_PASS=password
+
