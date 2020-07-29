acbcb7ea717da9eeb2343d0d621af82c3901e0e0
raspberrypisig <raspberrypisig@gmail.com>
2020-07-29 00:22:22 -0400

    Update set_env_vars.sh

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -7,6 +7,7 @@ export ADMIN_PASS=password
 export CINDER_DBPASS=password
 export CINDER_PASS=password
 export DASH_DBPASS=password
+export DEMO_USER=demo
 export DEMO_PASS=password
 export GLANCE_DBPASS=password
 export GLANCE_PASS=password
@@ -28,6 +29,5 @@ export CONTROLLER_PROVIDER_CIDR=192.168.20.0/24
 
 export COMPUTE_MANAGEMENT_IP=192.168.20.98
 export COMPUTE_PROVIDER_IP=192.168.20.98
-
 
 
