a2512b95f378d6c0aa015390003816d988298c78
raspberrypisig <raspberrypisig@gmail.com>
2020-07-29 02:44:19 -0400

    Update 102-create-project.sh

--- openstack-controller/102-create-project.sh
+++ openstack-controller/102-create-project.sh
@@ -3,3 +3,5 @@
 openstack project create --domain default --description "Service Project" $SERVICE_PROJECT
 openstack project create --domain default --description "Demo Project" $DEMO_PROJECT
 openstack user create --domain default --password $DEMO_PASS $DEMO_USER
+openstack role create $DEMO_ROLE
+openstack role add --project $DEMO_PROJECT --user $DEMO_USER $DEMO_ROLE
