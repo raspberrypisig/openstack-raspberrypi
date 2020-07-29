34051505afe54340093d3f405d035f98cedd21d1
raspberrypisig <raspberrypisig@gmail.com>
2020-07-28 22:14:32 -0400

    Update 009-memcache.sh

--- openstack-controller/009-memcache.sh
+++ openstack-controller/009-memcache.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i 's/127\.0\.0\.1/192\.168\.20\.99/' /etc/memcached.conf
+sed -i "s/127\.0\.0\.1/$CONTROLLER_MANAGEMENT_IP/" /etc/memcached.conf
 service memcached restart
 
 
