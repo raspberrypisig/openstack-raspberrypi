63ecba5df61e8e4405480f00c2a178bfd1e76197
raspberrypisig <raspberrypisig@gmail.com>
2020-07-29 03:50:59 -0400

    Create 103-glance.sh

---
+++ openstack-controller/103-glance.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mysql <<EOF
+CREATE DATABASE glance;
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY '$GLANCE_DBPASS';
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY '$GLANCE_DBPASS';
+EOF
+
+openstack user create --domain default --password $GLANCE_PASS glance
+openstack role add --project service --user glance admin
+openstack service create --name glance --description "OpenStack Image" image
+
+openstack endpoint create --region RegionOne image public http://controller:9292
+openstack endpoint create --region RegionOne image internal http://controller:9292
+openstack endpoint create --region RegionOne image admin http://controller:9292
+
+apt install -y glance
+
