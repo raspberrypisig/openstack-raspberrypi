cf0c987a49f0edb19e6ab8a09d09213bbb578295
raspberrypisig <raspberrypisig@gmail.com>
2020-07-29 05:25:42 -0400

    Update 105-nova.sh

--- openstack-controller/105-nova.sh
+++ openstack-controller/105-nova.sh
@@ -13,3 +13,17 @@ GRANT ALL PRIVILEGES ON nova_cell0.* TO 'nova'@'%' IDENTIFIED BY '$NOVA_DBPASS';
 
 EOF
 
+openstack user create --domain default --password $NOVA_PASS nova
+openstack role add --project service --user nova admin
+openstack service create --name nova --description "OpenStack Compute" compute
+
+openstack endpoint create --region RegionOne compute public http://controller:8774/v2.1
+openstack endpoint create --region RegionOne compute admin http://controller:8774/v2.1
+
+apt install -y nova-api nova-conductor nova-novncproxy nova-scheduler
+
+
+
+
+
+
