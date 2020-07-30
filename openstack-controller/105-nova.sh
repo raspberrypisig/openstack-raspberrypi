c45d048c410e446252b8d1501659364ffc2dcbc3
raspberrypisig <raspberrypisig@gmail.com>
2020-07-30 02:29:43 -0400

    Update 105-nova.sh

--- openstack-controller/105-nova.sh
+++ openstack-controller/105-nova.sh
@@ -30,6 +30,19 @@ sed -i  "/^\[database\]/a connection = mysql+pymysql://nova:NOVA_DBPASS@controll
 
 sed -i  "/^\[DEFAULT\]/a transport_url = rabbit://openstack:RABBIT_PASS@controller:5672/" /etc/nova/nova.conf
 
+sed -i '/^\[api\]/a auth_strategy = keystone' /etc/nova/nova.conf
+
+sed -i "/^\[keystone_authtoken\]/a \
+www_authenticate_uri = http://controller:5000/\n\
+auth_url = http://controller:5000/\n\
+memcached_servers = controller:11211\n\
+auth_type = password\n\
+project_domain_name = Default\n\
+user_domain_name = Default\n\
+project_name = service\n\
+username = nova\n\
+password = NOVA_PASS\
+" /etc/nova/nova.conf
 
 sed -i "//a \
 region_name = RegionOne\n\
